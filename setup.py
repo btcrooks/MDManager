dfca61c832b440187ce7ac34c2f7147b5d1cae71
Brandon Crooks <crookrb@hotmail.com>
2016-01-18 18:20:38 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+  name='MDdb',
+  version='1.0.0',
+  description='TODO',
+  url='https://github.com/btcrooks/MDdb.git',
+  author='Brandon Crooks',
+  author_email='crookrb@hotmail.com',
+  license='MIT',
+  packages=['lib'],
+  zip_safe=False
+)
